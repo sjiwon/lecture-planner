2e45e12ee7bc0ba566d5d4f71117437ec29de44b
SeoJiWon <sjiwon4491@gmail.com>
2023-05-07 16:39:40 +0900

    init commit

---
+++ src/main/java/com/sjiwon/lectureplanner/LectureplannerApplication.java
@@ -0,0 +1,11 @@
+package com.sjiwon.lectureplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LectureplannerApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(LectureplannerApplication.class, args);
+	}
+}
