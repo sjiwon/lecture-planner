99e88366da0f160031d53a6bdd67779f42bec843
SeoJiWon <sjiwon4491@gmail.com>
2023-05-07 17:51:30 +0900

    feat: p6spy 추가

--- src/main/java/com/sjiwon/lectureplanner/LecturePlannerApplication.java
+++ src/main/java/com/sjiwon/lectureplanner/LecturePlannerApplication.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LectureplannerApplication {
+public class LecturePlannerApplication {
 	public static void main(String[] args) {
-		SpringApplication.run(LectureplannerApplication.class, args);
+		SpringApplication.run(LecturePlannerApplication.class, args);
 	}
 }
