652f2216a765ecb26a29395aa8f62c0aa7d92e1e
SeoJiWon <sjiwon4491@gmail.com>
2023-05-07 17:12:12 +0900

    feat: Student 학생 도메인

---
+++ src/main/java/com/sjiwon/lectureplanner/student/domain/Student.java
@@ -0,0 +1,30 @@
+package com.sjiwon.lectureplanner.student.domain;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+@Entity
+@Table(name = "student")
+public class Student {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    private int grade;
+
+    private Student(String name, int grade) {
+        this.name = name;
+        this.grade = grade;
+    }
+
+    public static Student createStudent(String name, int grade) {
+        return new Student(name, grade);
+    }
+}
